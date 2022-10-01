41e16023209341dda5fb2df2c5db831bfcd058cf
jdvega21 <jdvega21@hotmail.com>
2022-09-30 20:19:18 -0600

    API added to display products, context and cart added

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,25 +1,23 @@
-import React, {useState} from "react";
+import React, {useContext} from "react";
 import "@styles/ProductItem.scss";
-import addToCart from '@icons/bt_add_to_cart.svg';
+import AppContext from '@context/AppContext';
+import addToCartImage from '@icons/bt_add_to_cart.svg';
 
-const ProductItem = () => {
-	const [cart, setCard] = useState([]);
-	const handleClick = () => {
-		setCard([]);
+const ProductItem = ({product}) => {
+	const {addToCart} = useContext(AppContext);
+	const handleClick = item => {
+		addToCart(item);
 	}
 	return (
 		<div className="ProductItem">
-			<img
-				src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-				alt=""
-			/>
+			<img src={product.images[0]} alt={product.title}/>
 			<div className="ProductItem-info">
 				<div>
-					<p>$120,00</p>
-					<p>Bike</p>
+					<p>${product.price}</p>
+					<p>{product.title}</p>
 				</div>
-				<figure onClick={handleClick}>
-					<img src={addToCart} alt="add-to-card" />
+				<figure onClick={() => handleClick(product)}>
+					<img src={addToCartImage} alt="add-to-card" />
 				</figure>
 			</div>
 		</div>
