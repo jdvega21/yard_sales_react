bf44e81098824c817308a59c7350cc68d452f85d
jdvega21 <jdvega21@hotmail.com>
2022-10-01 19:24:30 -0600

    Logic to delete products from cart

--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -18,8 +18,8 @@ const MyOrder = () => {
 				<p className="title">My order</p>
 			</div>
 			<div className="my-order-content">
-				{state.cart.map(product => (
-					<OrderItem product={product} key={`orderItem-${product.id}`} ></OrderItem>
+				{state.cart.map((product,index) => (
+					<OrderItem product={product} key={`orderItem-${index}`} indexValue={index} ></OrderItem>
 				))}
 				<div className="order">
 					<p>
