8a3643935f2f56ca0214c68a364ddd4b1454b6f4
jdvega21 <jdvega21@hotmail.com>
2022-10-05 17:47:42 -0600

    [FIX] Clean logs display

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -8,7 +8,6 @@ const API = 'https://api.escuelajs.co/api/v1/products';
 
 const ProductList = () => {
 	const products = useGetProducts(API);
-	console.log(products);
 	return (
 		<section className="main-container">
 			<div className="ProductList">
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,7 +12,6 @@ const Login = () => {
 			usename: formData.get('email'),
 			password: formData.get('password')
 		}
-		console.log(data);
 	}
 
 	return (
